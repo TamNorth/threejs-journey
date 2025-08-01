1161b0b5a3f1fc10c487cc18a24b39fbe37e9a2b
Demim0nde <tamnorth.github.37eup@slmails.com>
2025-08-01 15:58:41 +0100

    change vite config publicDir to /public and move over texture assets

--- vite.config.js
+++ vite.config.js
@@ -1,9 +1,9 @@
 import restart from "vite-plugin-restart";
 
 export default {
-  base: process.env.NODE_ENV === "production" ? "/threejs-journey/" : "./",
+  base: "./",
   root: "src/", // Sources files (typically where index.html is)
-  publicDir: "../static/", // Path from "root" to static assets (files that are served as they are)
+  publicDir: "../public/", // Path from "root" to static assets (files that are served as they are)
   server: {
     host: true, // Open to local network and display URL
     open: !("SANDBOX_URL" in process.env || "CODESANDBOX_HOST" in process.env), // Open if it's not a CodeSandbox
