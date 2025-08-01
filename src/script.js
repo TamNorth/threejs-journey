b182b2d8620d429a9df3b37df60f047067591000
Demim0nde <tamnorth.github.37eup@slmails.com>
2025-08-01 13:07:42 +0100

    fix 404 error on loading fonts by moving fonts to /public folder

--- src/script.js
+++ src/script.js
@@ -24,14 +24,34 @@ const matcapTexture = textureLoader.load("textures/matcaps/8.png");
 matcapTexture.colorSpace = THREE.SRGBColorSpace;
 
 /**
+ * Objects
+ */
+
+// Material
+const material = new THREE.MeshMatcapMaterial({ matcap: matcapTexture });
+
+// Donuts
+const donutGeometry = new THREE.TorusGeometry(0.3, 0.2, 32, 64);
+
+for (let i = 0; i < 100; i++) {
+  const donut = new THREE.Mesh(donutGeometry, material);
+  donut.position.x = (Math.random() - 0.5) * 10;
+  donut.position.y = (Math.random() - 0.5) * 10;
+  donut.position.z = (Math.random() - 0.5) * 10;
+  donut.rotation.x = Math.random() * Math.PI;
+  donut.rotation.y = Math.random() * Math.PI;
+  const scale = Math.random();
+  donut.scale.set(scale, scale, scale);
+
+  scene.add(donut);
+}
+
+/**
  * Fonts
  */
 const fontLoader = new FontLoader();
 
-fontLoader.load("/fonts/helvetiker_regular.typeface.json", (font) => {
-  // Material
-  const material = new THREE.MeshMatcapMaterial({ matcap: matcapTexture });
-
+fontLoader.load("/public/fonts/helvetiker_regular.typeface.json", (font) => {
   // Text
   const textGeometry = new TextGeometry("Hello Three.js", {
     font: font,
@@ -48,22 +68,6 @@ fontLoader.load("/fonts/helvetiker_regular.typeface.json", (font) => {
 
   const text = new THREE.Mesh(textGeometry, material);
   scene.add(text);
-
-  // Donuts
-  const donutGeometry = new THREE.TorusGeometry(0.3, 0.2, 32, 64);
-
-  for (let i = 0; i < 100; i++) {
-    const donut = new THREE.Mesh(donutGeometry, material);
-    donut.position.x = (Math.random() - 0.5) * 10;
-    donut.position.y = (Math.random() - 0.5) * 10;
-    donut.position.z = (Math.random() - 0.5) * 10;
-    donut.rotation.x = Math.random() * Math.PI;
-    donut.rotation.y = Math.random() * Math.PI;
-    const scale = Math.random();
-    donut.scale.set(scale, scale, scale);
-
-    scene.add(donut);
-  }
 });
 
 /**
