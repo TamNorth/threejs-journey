5650a5537be1797ce529effcb99107f570bccc13
Demim0nde <tamnorth.github.37eup@slmails.com>
2025-08-13 12:27:45 +0100

    set alphaWrite to false to prevent adjusting of particle visibility according to subsequently-rendered particles

--- chapter_02/17-particles/src/script.js
+++ chapter_02/17-particles/src/script.js
@@ -27,28 +27,30 @@ const particleTexture = textureLoader.load("./textures/particles/2.png");
 const particleMaterial = new THREE.PointsMaterial({
   color: "cyan",
   alphaMap: particleTexture, // Problem with this method alone is that it depends on the draw order - e.g. if a particle in front of another is rendered first and the alpha is 0, the pixels of the particle behind will be invisible too as the alpha map will be applied to both
-  //   alphaTest: 0.001, // This is better, as the alphaTest simply tells the GPU not to render parts of the material if their alphaMap value is below a certain threshold (in conjunction with alphaMap partial transparency) - however, it has a hard cut-off, so the same problems as before will occur in some cases, just at a different threshold - so it doesn't work great for partial transparency
+  // alphaTest: 0.001, // This is better, as the alphaTest simply tells the GPU not to render parts of the material if their alphaMap value is below a certain threshold (in conjunction with alphaMap partial transparency) - however, it has a hard cut-off, so the same problems as before will occur in some cases, just at a different threshold - so it doesn't work great for partial transparency
   transparent: true,
   size: 0.1,
   sizeAttenuation: true,
 });
+// particleMaterial.depthTest = false; // This prevents WebGL from trying to order particles according to whether they are nearer or further from the camera, and avoids the initial issue with particles behind being affected by the alphaMap of particles in front. However it will cause bugs when other materials with different colours, etc., are introduced to the scene, as particles behind an object will be rendered as if in front of it
+particleMaterial.depthWrite = false; // Instead of telling WebGL not to look in the depth buffer to test whether what's being drawn is closer than what's already been drawn, we can just tell it not to write in it when drawing an object (i.e. it will not go back and modify objects behind?). This is generally a good solution
 
 // Geometry
-// const particleGeometry = new THREE.SphereGeometry(1, 32, 32);
+// const particlesGeometry = new THREE.SphereGeometry(1, 32, 32);
 const particleCount = 5000;
-const particleGeometry = new THREE.BufferGeometry();
-const vertices = [];
+const particlesGeometry = new THREE.BufferGeometry();
+const vertices = new Float32Array(particleCount * 3);
 for (let i = 0; i < particleCount * 3; i++) {
-  vertices.push((Math.random() - 0.5) * 10);
+  vertices[i] = (Math.random() - 0.5) * 10;
 }
-const verticesTyped = new Float32Array(vertices);
-particleGeometry.setAttribute(
+
+particlesGeometry.setAttribute(
   "position",
-  new THREE.BufferAttribute(verticesTyped, 3)
+  new THREE.BufferAttribute(vertices, 3)
 );
 
 // Points
-const particles = new THREE.Points(particleGeometry, particleMaterial);
+const particles = new THREE.Points(particlesGeometry, particleMaterial);
 scene.add(particles);
 
 /**
